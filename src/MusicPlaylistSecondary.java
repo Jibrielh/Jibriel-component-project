deef28ad55424d634cd4f3632179adb66a2fc196
Jibrielh <jibrielh939@gmail.com>
2025-11-12 12:16:16 -0500

    y

---
+++ src/MusicPlaylistSecondary.java
@@ -0,0 +1,12 @@
+
+public abstract class MusicPlaylistSecondary implements MusicPlaylist {
+
+    public Boolean contains(String s) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
